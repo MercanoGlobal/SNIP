524d9fa165be0980add2cf75d35dc8ff2450f1b8
MercanoGlobal <mercanoglobal@gmail.com>
2024-09-19 22:50:38 +0300

    Missed commit https://github.com/MercanoGlobal/SNIP/commit/52c91062123c22158cb10634de63c62aa8a0aa69
    Who designs themes with two separate footers?! Who?!

--- htdocs/themes/stikkedizr/views/view/view_footer.php
+++ htdocs/themes/stikkedizr/views/view/view_footer.php
@@ -15,6 +15,7 @@ if(isset($ace_modes)){
 //Javascript
 $this->carabiner->js('jquery.js');
 $this->carabiner->js('jquery.timers.js');
+$this->carabiner->js('bootstrap.min.js');
 $this->carabiner->js('crypto-js/rollups/aes.js');
 $this->carabiner->js('lz-string-1.3.3-min.js');
 $this->carabiner->js('filereader.js');
