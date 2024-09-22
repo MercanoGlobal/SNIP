1842a3d2cd0d27557b92dc23199875bd326bf279
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2024-09-22 12:29:22 +0200

    Update footer_message.php
    Silly me! Use `<p></p>` so I don't need the `<br>`

--- htdocs/themes/geocities/views/defaults/footer_message.php
+++ htdocs/themes/geocities/views/defaults/footer_message.php
@@ -1,7 +1,7 @@
 <footer class="footer">
 	<?php if ($this->config->item('alert_footer')) { ?>
 		<!-- Alert Footer -->
-		<?php echo lang('alert_footer'); ?><br>
+		<p></p><?php echo lang('alert_footer'); ?></p>
 	<?php } ?>
 	<?php echo lang('powered_by'); ?> <a href="<?php echo proj_url(); ?>"><?php echo proj_name(); ?></a>
     <br />
