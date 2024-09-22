ec93f123f092407550c7c6441b669e0143e7aa50
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2024-09-22 12:08:58 +0200

    Update footer_message.php
    Add footer message

--- htdocs/themes/bootstrap4/views/defaults/footer_message.php
+++ htdocs/themes/bootstrap4/views/defaults/footer_message.php
@@ -1,3 +1,7 @@
 <div class="footer">
+    <?php if ($this->config->item('alert_footer')) { ?>
+		<!-- Alert Footer -->
+		<?php echo lang('alert_footer'); ?><br>
+	<?php } ?>
     <?php echo lang('powered_by'); ?> <a href="<?php echo proj_url(); ?>"><?php echo proj_name(); ?></a>
 </div>
